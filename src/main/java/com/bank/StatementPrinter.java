b78fc1f3bb425fd700fadc910b8bd176a07df60a
Brahim <brahim.oml@gmail.com>
2026-02-11 20:09:17 +0100

    test

--- src/main/java/com/bank/StatementPrinter.java
+++ src/main/java/com/bank/StatementPrinter.java
@@ -1,21 +1,27 @@
 package com.bank;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class StatementPrinter {
+    private static final DateTimeFormatter DATE_FORMAT =
+            DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public void print(List<Transaction> transactions) {
-        System.out.println("DATE | AMOUNT | BALANCE");
-
+        System.out.println("DATE       | AMOUNT  | BALANCE");
         for (Transaction transaction : transactions) {
-            System.out.println(this.format(transaction));
+            System.out.println(format(transaction));
         }
     }
 
-    private static String format(Transaction transaction) {
-        return transaction.getDate()
+    private String format(Transaction transaction) {
+        String amount = transaction.getAmount() >= 0
+                ? "+" + transaction.getAmount()
+                : String.valueOf(transaction.getAmount());
+
+        return transaction.getDate().format(DATE_FORMAT)
                 + " | "
-                + transaction.getAmount()
+                + amount
                 + " | "
                 + transaction.getBalance();
     }
